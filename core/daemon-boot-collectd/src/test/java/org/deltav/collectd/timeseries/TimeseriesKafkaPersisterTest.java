084ac5dfac855110cb3dc4864aff605c40cbd860
David Hustace <david@beaconstrategists.com>
2026-04-19 11:19:55 -0400

    test(collectd): remove unused assertThat import from TimeseriesKafkaPersisterTest

--- core/daemon-boot-collectd/src/test/java/org/deltav/collectd/timeseries/TimeseriesKafkaPersisterTest.java
+++ core/daemon-boot-collectd/src/test/java/org/deltav/collectd/timeseries/TimeseriesKafkaPersisterTest.java
@@ -16,7 +16,6 @@
  */
 package org.deltav.collectd.timeseries;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
