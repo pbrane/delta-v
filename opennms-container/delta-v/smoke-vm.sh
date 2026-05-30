f30a28c64f15280725f1868d7e7cc42070a46134
David Hustace <david@beaconstrategists.com>
2026-05-30 19:31:14 +0200

    chore(smoke): bump smoke-vm.sh to v1.3.0-rc5

--- opennms-container/delta-v/smoke-vm.sh
+++ opennms-container/delta-v/smoke-vm.sh
@@ -23,8 +23,8 @@ docker system prune -a --volumes -f
 rm -rf ~/delta-v-smoke
 mkdir -p ~/delta-v-smoke && cd ~/delta-v-smoke
 
-GIT_REF=v1.3.0-rc3
-IMG_TAG=1.3.0-rc3
+GIT_REF=v1.3.0-rc5
+IMG_TAG=1.3.0-rc5
 
 BASE=https://raw.githubusercontent.com/pbrane/delta-v/$GIT_REF/opennms-container/delta-v
 
